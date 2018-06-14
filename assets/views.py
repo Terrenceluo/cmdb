8d44289cc9107396cb3783be45fbb90ed1205cd1
lxt <terrence.luo@coassets.com.cn>
2018-06-14 13:51:29 +0800

    新资产待审批区

--- assets/views.py
+++ assets/views.py
@@ -1,16 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+
 from django.http import HttpResponse
 from django.utils.translation import ugettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 
+from assets import models, asset_handler
+
 
 @csrf_exempt
 def report(request):
-    if request.method == 'POST':
+    """
+    通过csrf_exempt装饰器，跳过Django的csrf安全机制，让post的数据能被接收，但这又会带来新的安全问题。
+    可以在客户端，使用自定义的认证token，进行身份验证。这部分工作，请根据实际情况，自己进行。
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
         asset_data = request.POST.get('asset_data')
-        print(asset_data)
-        return HttpResponse(_('Receive data successfully'))
+        data = json.loads(asset_data)
+        # 各种数据检查，请自行添加和完善！
+        if not data:
+            return HttpResponse(_('Data not found'))
+        if not issubclass(dict, type(data)):
+            return HttpResponse(_('Data must be dict'))
+        # 是否携带了关键的sn号
+        sn = data.get('sn', None)
+        if sn:
+            # 进入审批流程
+            # 首先判断是否在上线资产中存在该sn
+            asset_obj = models.Asset.objects.filter(sn=sn)
+            if asset_obj:
+                # 进入已上线资产的数据更新流程
+                pass
+                return HttpResponse(_('Asset data has updated'))
+            else:  # 如果已上线资产中没有，那么说明是未批准资产，进入新资产待审批区，更新或者创建资产。
+                obj = asset_handler.NewAsset(request, data)
+                response = obj.add_to_new_assets_zone()
+                return HttpResponse(response)
+        else:
+            return HttpResponse(_('Serial number not found, please check data'))
     else:
         return HttpResponse(_('Only post method'))
