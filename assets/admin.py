ae4a5ce06d0edcb92daff02d516f311b36bb3f5e
lxt <terrence.luo@coassets.com.cn>
2018-06-14 14:46:14 +0800

    审批新资产

--- assets/admin.py
+++ assets/admin.py
@@ -3,13 +3,31 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from assets import models
+from assets import models, asset_handler
+from django.utils.translation import ugettext_lazy as _
 
 
 class NewAssetAdmin(admin.ModelAdmin):
     list_display = ['asset_type', 'sn', 'model', 'manufacturer', 'c_time', 'm_time']
     list_filter = ['asset_type', 'manufacturer', 'c_time']
     search_fields = ('sn',)
+
+    actions = ['approve_selected_new_assets']
+
+    def approve_selected_new_assets(self, request, queryset):
+        # 获得被打钩的checkbox对应的资产
+        selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+        success_upline_number = 0
+        for asset_id in selected:
+            obj = asset_handler.ApproveAsset(request, asset_id)
+            ret = obj.asset_upline()
+            if ret:
+                success_upline_number += 1
+        # 顶部绿色提示信息
+        self.message_user(request, '%s  %s  %s！' %
+                          (_('Success to approve'), success_upline_number, _('new assets upline')))
+
+    approve_selected_new_assets.short_description = _('Approve selected new assets')
 
 
 class AssetAdmin(admin.ModelAdmin):
