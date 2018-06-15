b305bb5bbd032083c211f7340378e95fade17ac0
terrence <terrence.luo@coassets.com.cn>
2018-06-15 21:35:38 +0800

    添加 client

---
+++ client/conf/settings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import os
+
+# 远端服务器配置
+Params = {
+    'server': '127.0.0.1',
+    'port': 80,
+    'url': '/assets/report/',
+    'request_timeout': 30,
+}
+
+# 日志文件配置
+
+PATH = os.path.join(os.path.dirname(os.getcwd()), 'log', 'cmdb.log')
+
+# 更多配置，请都集中在此文件中
